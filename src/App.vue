bb97596f9ecc538e47d7aca409588cadb8bec641
KICHA <kicaUBUNTU@gmail.com>
2017-09-16 14:08:44 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <app-head></app-head>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Home from "./components/Home.vue";
+import Cv from "./components/Cv.vue";
+import BehindData from "./components/BehindData.vue";
+import Projects from "./components/Projects.vue";
+import Contact from "./components/contact.vue";
+
+
+export default {
+
+  components: {
+    "app-head":Header,
+    "Home":Home,
+    "Cv":Cv,
+    "BehindData":BehindData,
+    "Projects":Projects
+  },
+  data () {
+
+
+  },
+    methods:{
+
+    }
+  }
+
+</script>
+
+<style>
+body
+
+{
+  padding:0px;
+  width:100%;
+  height:100%;
+  margin:0%;
+}
+
+
+
+
+</style>
